6e4f8b93aa8634fc8caa7dc77cb723d5293a8518
Andrey Belskiy <diksandab@gmail.com>
2021-02-01 07:51:15 +0600

    Some frontend. Added entry page

--- vsearch.py
+++ vsearch.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def entry_page() -> 'str':
-    return "Hello"
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                            the_title='Welcome to vsearch',)
 
 
 if __name__ == '__main__':
